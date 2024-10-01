d683cbb0f707ccff77f79212bcfd6f8eccb47a02
DanielaAcoH <danihdez.aco@gmail.com>
2024-09-30 22:24:53 -0600

    DAN|70|Correcióndeiconosybotones

--- App/components/PerfilUsuarioTab.js
+++ App/components/PerfilUsuarioTab.js
@@ -1,35 +1,60 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { Center, Button, HStack, CheckIcon, Text, Divider, NativeBaseProvider, Box } from 'native-base';
+import { Center, Button, HStack, Text, Divider, NativeBaseProvider, Box } from 'native-base';
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export function PerfilUsuarioTab() {
+export function PerfilUsuarioTab({navigation}) {
+
+    const menuUsuario = [ 
+    { name: "Mensajes", icon: <MaterialIcons name="message" size={20} color="green" marginTop={11} />, screen: "MensajesScreen" },
+    { name: "Notificaciones", icon: <Ionicons name="notifications" size={20} color="blue" marginTop={9}/>, screen: "NotificacionesScreen" },
+    { name: "Monedero", icon: <AntDesign name="wallet" size={20} color="purple" marginTop={9}/>, screen: "MonederoScreen" },
+    { name: "Mi Nivel", icon: <MaterialIcons name="star" size={20} color="gold" marginTop={9}/>, screen: "NivelScreen" },
+    { name: "Mis Invitaciones", icon: <Ionicons name="people" size={20} color="orange" marginTop/>, screen: "InvitacionesScreen" },
+    { name: "Galeria", icon: <MaterialIcons name="photo-library" size={20} color="red" />, screen: "GaleriaScreen" },
+    ] 
+
   return (
     <NativeBaseProvider>
       <Center>
         <Box
           size={120}
           bgColor="gray.200"
-          borderRadius="full"
-          mb={5}
+          borderRadius="full"         
         />
       </Center>
 
-      <Divider w="100%" bg="pink.500" />
+      <Center><Text bold>#usuario</Text></Center>   
+     <HStack space={3} justifyContent="center" marginTop={2}>
+      <Center><Text bold right={6}>Pais</Text></Center>
+      <Center><Text bold left={35}>Idioma</Text></Center>
+    </HStack>
+
+    <HStack space={3} justifyContent="center" marginTop={3}>
+      <Center><Text bold right={5}>#amigos</Text></Center>
+      <Center><Text bold left={2}>#seguidores</Text></Center>
+      <Center><Text bold left={6}>#siguiendo</Text></Center>
+    </HStack>
+
+      <Divider w="100%" bg="pink.500" top={2} />
 
       <Box alignItems="flex-start" mb={10} px={6}>
-        {["Mensajes", "Notificaciones", "Monedero", "Mi Nivel", "Mis Invitaciones", "Galeria"].map((item, index) => (
-          <HStack key={index} mt={2}>
-            <CheckIcon size="5" mt="2.5" color="emerald.500" />
-            <Button variant="unstyled" onPress={() => {}}>
-              {item}
-            </Button>
-          </HStack>
-        ))}
+              {menuUsuario.map((item, index) => (
+                <HStack key={index} mt={"1%"}>
+                  {item.icon}
+                  <Button variant="unstyled" onPress={() => navigation.navigate(item.screen)}>
+                    {item.name}
+                  </Button>
+                </HStack>
+              ))}
       </Box>
 
-      <Box alignItems="flex-start" mb={4}>
+
+      <Box alignItems="flex-start">
         <Button borderRadius={60} bgColor="#E01983" width="80%" alignSelf="center" onPress={() => {}}>
           Verificar
         </Button>
@@ -38,16 +63,3 @@ export function PerfilUsuarioTab() {
   );
 }
 export default PerfilUsuarioTab;
-
-/*
-const Drawer = createDrawerNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={PerfilUsuarioTab} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}*/
